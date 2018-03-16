186f2f1d25720b5ee67a435ab64c7f6bee9cfbc9
klaus <klauszhou@wezhuiyi.com>
2018-03-16 17:47:18 +0800

    add status

--- src/main/java/org/zhuiyi/yiconnect/data/transfer/Application.java
+++ src/main/java/org/zhuiyi/yiconnect/data/transfer/Application.java
@@ -8,12 +8,21 @@ import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletCont
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.zhuiyi.yiconnect.data.transfer.dto.response.base.R;
 
 @EnableAutoConfiguration
 @ComponentScan(basePackages = { "org.zhuiyi.yiconnect.data.transfer" })
 @SpringBootApplication
+@RestController
 public class Application extends SpringBootServletInitializer implements EmbeddedServletContainerCustomizer {
 
+	@GetMapping("/status")
+	public R status() {
+		return R.ok();
+	}
+	
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(Application.class);
